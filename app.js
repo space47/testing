b5279fe02bd2a32789c81201d7323caa81d1b9fd
space47 <varnitgupta47@gmail.com>
2023-07-10 18:12:38 +0530

    initial2

--- app.js
+++ app.js
@@ -46,20 +46,20 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload())
 
 // swagger
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+// const swaggerUI = require('swagger-ui-express')
+// const YAML = require('yamljs')
+// const swaggerDocument = YAML.load('./swagger.yaml')
 
 // route
 app.get("/", (req, res) => {
   res.send("E commerce");
 });
 
-app.get('/a',(req,res)=> {
-  res.send('<h1>E-Comm</h1><a href = "/api-docs">Document</a>')
-})
+// app.get('/a',(req,res)=> {
+//   res.send('<h1>E-Comm</h1><a href = "/api-docs">Document</a>')
+// })
 
-app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+// app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.get("/api/v1", (req, res) => {
   // if signed during creation then have to use .signedcookie
